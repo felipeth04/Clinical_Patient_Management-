10c3f28f100cbb0747d504483e9d97b64cb54c92
Felipe Herrera <felipeth4@hotmail.com>
2022-09-23 15:31:30 -0500

    Welcome message

---
+++ clinicalApp.py
@@ -0,0 +1,7 @@
+print("***************************************************************************")
+print("WELCOME TO CLINIC APP, THE BEST APPLICATION FOR THE MANAGEMENT OF YOUR PATIENTS")
+print("***************************************************************************")
+
+
+ 
+
